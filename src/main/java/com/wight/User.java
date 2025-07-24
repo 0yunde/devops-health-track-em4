9e491a6abae41f01080938bb5f8b8e84556a5a1f
Felipe Gutiérrez <caracoloro@gmail.com>
2025-07-24 14:47:52 -0400

    feat: chnage from spanish to english

--- src/main/java/com/wight/User.java
+++ src/main/java/com/wight/User.java
@@ -1,5 +1,28 @@
 package com.wight;
 
 public class User {
-    
+    private String username;
+    private double weight;
+
+    public User(String username, double weight) {
+        this.username = username;
+        this.weight = weight;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void updateWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+
+    public void showInformation() {
+        System.out.println("Usuario: " + username + ", Peso Actual: " + weight + " kg");
+    }
 }
+
